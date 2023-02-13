7b398cc18a736f419cff95696eb9958530af3a53
li4639 <346766377@qq.com>
2023-02-13 13:25:12 +0800

    fix ui bug

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -9,7 +9,7 @@ export function Header(p:{props: any}) {
   return (
     <div className={classNames(sticky?'sticky':'',"w-full z-10 flex justify-between items-center top-0 px-32 py-3 md:px-5",isTransparent?'bg-transparent':'bg-white',className)}>
       <Link href="/">
-        <img className="block w-16" alt="" src="/images/logo.svg"/>
+        <img className="block w-16 h-11" alt="" src="/images/logo.svg"/>
       </Link>
       <Navigation />
     </div>
