8be6649b3a92484a688179a54d361f7a9468debe
lijun4639 <346766377@qq.com>
2023-03-27 14:13:41 +0800

    fix ui bug

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -108,7 +108,7 @@ export function Footer() {
               {
                 navList.map((v,i)=>{
                   return(
-                    <li key={`${i}`} className={classNames('md:mt-5',i===(navList.length-1)?'mr-0':'mr-[3.75rem]')}>
+                    <li key={`${i}`} className={classNames('md:mt-5',i===(navList.length-1)?'mr-0':'mr-[3.75rem] md:mr-2')}>
                       {
                         v.href && !v.children?
                           <Link className="text-2xl font-bold md:text-xl" href={v.href}>{v.name}</Link>
