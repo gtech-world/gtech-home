a47f866980be247caadfaffbb3847e8334fb06dc
lijun4639 <346766377@qq.com>
2023-04-24 17:05:25 +0800

    updated qrcode

--- components/qrGroup.tsx
+++ components/qrGroup.tsx
@@ -5,14 +5,16 @@ export function QrGroup(p: any) {
   const { qrText,className } = p
   return (
     <div className={classNames('w-[18.875rem] mx-auto md:w-full md:max-w-[22rem]',className)}>
-      <div className={classNames("py-2 rounded-[2.25rem] outline outline-[5px] outline-green outline-offset-4 border-[1.75px] border-green flex justify-center items-center mr-[8px]")}>
-        <div className="border-r-[3px] border-green-2 pr-5 mr-5 flex flex-col justify-between h-full">
-          <img className="mb-2" src="/images/carbon3_name.svg" alt=""/>
-          <img className="w-[6.25rem]" src="/images/earth.png" alt=""/>
-        </div>
-        <div className="w-[7.125rem] flex flex-col items-center">
-          <p className="text-[0.6875rem] text-center mb-2 leading-[1rem] font-semibold">{qrText?qrText:'Product Carbon Footprint Certified by AIAG'}</p>
-          <img className="w-[5.75rem]" src="/images/qrcode.svg" alt=""/>
+      <div className="p-1 border-[4px] border-green rounded-[2.75rem]">
+        <div className={classNames("py-2 rounded-[2.25rem] border-[1.75px] border-green flex justify-center items-center")}>
+          <div className="border-r-[3px] border-green-2 pr-5 mr-5 flex flex-col justify-between h-full">
+            <img className="mb-2" src="/images/carbon3_name.svg" alt=""/>
+            <img className="w-[6.25rem]" src="/images/earth.png" alt=""/>
+          </div>
+          <div className="w-[7.125rem] flex flex-col items-center">
+            <p className="text-[0.6875rem] text-center mb-2 leading-[1rem] font-semibold">{qrText?qrText:'Product Carbon Footprint Certified by AIAG'}</p>
+            <img className="w-[5.75rem]" src="/images/qrcode.svg" alt=""/>
+          </div>
         </div>
       </div>
     </div>
