44f474b2942dc359fd61e95d77e731e55086f597
lijun4639 <346766377@qq.com>
2023-05-04 17:00:31 +0800

    updated img

--- components/qrGroup.tsx
+++ components/qrGroup.tsx
@@ -8,7 +8,7 @@ export function QrGroup(p: any) {
       <div className="p-1 border-[4px] border-green rounded-[2.75rem]">
         <div className={classNames("py-2 rounded-[2.25rem] border-[1.75px] border-green flex justify-center items-center")}>
           <div className="border-r-[3px] border-green-2 pr-5 mr-5 flex flex-col justify-between h-full">
-            <img className="mb-2" src="/images/carbon3_name.svg" alt=""/>
+            <img className="mb-2 md:w-[7.1rem]" src="/images/carbon3_name.svg" alt=""/>
             <img className="w-[6.25rem]" src="/images/earth.png" alt=""/>
           </div>
           <div className="w-[7.125rem] flex flex-col items-center">
