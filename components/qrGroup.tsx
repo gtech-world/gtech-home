dfcae644804a1af8323426303a9f29184d2bbc87
YongZL <1679963578@qq.com>
2023-07-17 14:25:12 +0800

    fix use Image

--- components/qrGroup.tsx
+++ components/qrGroup.tsx
@@ -1,5 +1,6 @@
 import React, {HTMLAttributes} from "react";
 import classNames from "classnames";
+import Image from "next/image";
 
 export function QrGroup(p: any) {
   const { qrText,className } = p
@@ -8,12 +9,12 @@ export function QrGroup(p: any) {
       <div className="p-1 border-[4px] border-green rounded-[2.75rem]">
         <div className={classNames("py-2 rounded-[2.25rem] border-[1.75px] border-green flex justify-center items-center")}>
           <div className="border-r-[3px] border-green-2 pr-5 mr-5 flex flex-col justify-between h-full">
-            <img className="mb-2 md:w-[7.1rem]" src="/images/carbon3_name.svg" alt=""/>
-            <img className="w-[6.25rem]" src="/images/earth.png" alt=""/>
+            <Image className="mb-2 md:w-[7.1rem]" src="/images/carbon3_name.svg" alt=""/>
+            <Image className="w-[6.25rem]" src="/images/earth.png" alt=""/>
           </div>
           <div className="w-[7.125rem] flex flex-col items-center">
             <p className="text-[0.6875rem] text-center mb-2 leading-[1rem] font-semibold">{qrText?qrText:'Product Carbon Footprint Certified by AIAG'}</p>
-            <img className="w-[5.75rem]" src="/images/qrcode.svg" alt=""/>
+            <Image className="w-[5.75rem]" src="/images/qrcode.svg" alt=""/>
           </div>
         </div>
       </div>
