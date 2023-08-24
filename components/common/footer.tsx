f47847c92c8598e4bb7eaf164827026efcfeb0e4
YongZL <1679963578@qq.com>
2023-08-24 14:34:51 +0800

    filing number

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,33 +1,37 @@
-import React, {useEffect, useMemo, useState} from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
-import {Trans, useTranslation} from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import classNames from "classnames";
-import {useNewsCate} from "@lib/hooks/useNewsCate";
+import { useNewsCate } from "@lib/hooks/useNewsCate";
 
-function SubNav(props:any){
-  const {className,data} = props
-  return(
-    <ul className={classNames('flex flex-col text-base mt-4 leading-[2.2] font-normal md:text-[0.9375rem]',className)}>
-      {
-        data.length && data.map((v:any,i:number)=>{
-          return(
-            v.render?v.render():
-              (
-                v.href?
-                  <Link href={v.href} className="link-hover" key={`${i}`}>{v.name}</Link>
-                  :
-                  <span>{v.name}</span>
-              )
-          )
-        })
-      }
+function SubNav(props: any) {
+  const { className, data } = props;
+  return (
+    <ul
+      className={classNames(
+        "flex flex-col text-base mt-4 leading-[2.2] font-normal md:text-[0.9375rem]",
+        className
+      )}
+    >
+      {data.length &&
+        data.map((v: any, i: number) => {
+          return v.render ? (
+            v.render()
+          ) : v.href ? (
+            <Link href={v.href} className="link-hover" key={`${i}`}>
+              {v.name}
+            </Link>
+          ) : (
+            <span>{v.name}</span>
+          );
+        })}
     </ul>
-  )
+  );
 }
 
 export function Footer() {
-  const {t,i18n} = useTranslation('common')
-  const cateList = useNewsCate()
+  const { t, i18n } = useTranslation("common");
+  const cateList = useNewsCate();
   // const navList = getNavList(t).concat([
   //   // @ts-ignore
   //   ])
@@ -37,70 +41,69 @@ export function Footer() {
     //   name: t('navigation.list.item1.name'),
     // },
     {
-      href: '/solutions',
-      name: t('navigation.list.item2.name'),
-      children:[
-        {
-          href: '/solutions/automotive',
-          name:
-            <Trans
-              i18nKey="2"
-              components={[<sup key="dsd"></sup>]}
-            >
-              {t('navigation.list.item2.children.child1.name')}
-            </Trans>,
-        },
-        {
-          href: '/solutions/web3',
-          name: t('navigation.list.item2.children.child2.name'),
-        },
-        {
-          href: '/solutions/governance',
-          name: t('navigation.list.item2.children.child3.name'),
-        },
-      ]
-    },
-    {
-      href: '/news',
-      name: t('navigation.list.item3.name'),
-      children: cateList.map(v=>{
-        return(
-          {
-            href: `/news?cateId=${v.id}`,
-            name: v.name,
-          }
-        )
-      })
-    },
-    {
-      href: '/contact',
-      name: t('navigation.list.item4.name'),
-      children:[
-        {
-          name: 'hi@gtech.world',
-        },
-        {
-          name: '(+86)021-66271287',
-        }
-      ]
-    },
-    {
-      name: t('navigation.list.item4.children.qrSubtitle'),
-      href:'',
+      href: "/solutions",
+      name: t("navigation.list.item2.name"),
       children: [
         {
-          render:()=>{
-            return(
-              <img className="w-[7.5rem] mt-2" src="/images/wechat_qrcode.png" alt=""/>
-            )
-          }
-        }
-      ]
-    }
-  ]
+          href: "/solutions/automotive",
+          name: (
+            <Trans i18nKey="2" components={[<sup key="dsd"></sup>]}>
+              {t("navigation.list.item2.children.child1.name")}
+            </Trans>
+          ),
+        },
+        {
+          href: "/solutions/web3",
+          name: t("navigation.list.item2.children.child2.name"),
+        },
+        {
+          href: "/solutions/governance",
+          name: t("navigation.list.item2.children.child3.name"),
+        },
+      ],
+    },
+    {
+      href: "/news",
+      name: t("navigation.list.item3.name"),
+      children: cateList.map((v) => {
+        return {
+          href: `/news?cateId=${v.id}`,
+          name: v.name,
+        };
+      }),
+    },
+    {
+      href: "/contact",
+      name: t("navigation.list.item4.name"),
+      children: [
+        {
+          name: "hi@gtech.world",
+        },
+        {
+          name: "(+86)021-66271287",
+        },
+      ],
+    },
+    {
+      name: t("navigation.list.item4.children.qrSubtitle"),
+      href: "",
+      children: [
+        {
+          render: () => {
+            return (
+              <img
+                className="w-[7.5rem] mt-2"
+                src="/images/wechat_qrcode.png"
+                alt=""
+              />
+            );
+          },
+        },
+      ],
+    },
+  ];
 
-  
-  if(i18n.language === 'en') navList.splice(1,1)
+  if (i18n.language === "en") navList.splice(1, 1);
   return (
     <div className="pb-5 bg-bgc md:px-8 md:py-12">
       <div className="flex flex-col mx-auto w-container md:w-full">
@@ -111,36 +114,61 @@ export function Footer() {
             {/*</div>*/}
             <ul className="flex mt-5 md:flex-col">
               <li className="mr-14 mt-[-1.3rem]">
-                <Link className="md:float-left" href="/" ><img className="w-20" src="/images/logo.svg" alt=""/></Link>
+                <Link className="md:float-left" href="/">
+                  <img className="w-20" src="/images/logo.svg" alt="" />
+                </Link>
               </li>
-              {
-                navList.map((v,i)=>{
-                  return(
-                    <li key={`${i}`} className={classNames('md:mt-5',i===(navList.length-1)?'mr-0':'mr-[3.75rem] md:mr-2')}>
-                      {
-                        v.href && !v.children?
-                          <Link className="text-xl font-bold link-hover md:text-xl" href={v.href}>{v.name}</Link>
-                          :
-                          <span className="text-xl font-bold md:text-xl">{v.name}</span>
-                      }
-                      {
-                        v.children && <SubNav data={v.children}/>
-                      }
-                    </li>
-                  )
-                })
-              }
+              {navList.map((v, i) => {
+                return (
+                  <li
+                    key={`${i}`}
+                    className={classNames(
+                      "md:mt-5",
+                      i === navList.length - 1 ? "mr-0" : "mr-[3.75rem] md:mr-2"
+                    )}
+                  >
+                    {v.href && !v.children ? (
+                      <Link
+                        className="text-xl font-bold link-hover md:text-xl"
+                        href={v.href}
+                      >
+                        {v.name}
+                      </Link>
+                    ) : (
+                      <span className="text-xl font-bold md:text-xl">
+                        {v.name}
+                      </span>
+                    )}
+                    {v.children && <SubNav data={v.children} />}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
         <div className="flex justify-between pt-4 mt-16 text-sm border-t border-black md:flex-col md:mt-8">
-
           <div>
             {/*<span>{t('footer.company')}</span>*/}
             {/*<a href="https://beian.miit.gov.cn/" target="_blank">您的备案号</a>*/}
-            <a rel="noreferrer" href="https://beian.miit.gov.cn/" target="_blank"> 沪ICP备2022024704号-2</a>
+            <a
+              rel="noreferrer"
+              href="https://beian.miit.gov.cn/"
+              target="_blank"
+            >
+              沪ICP备2023021387号-1
+            </a>
           </div>
-          <span onClick={()=>window.open(i18n.language === 'zh'?'/zhstatement':'/enstatement','_blank')} className="cursor-pointer md:mt-5 link-hover">{t('footer.viewBtn')}</span>
+          <span
+            onClick={() =>
+              window.open(
+                i18n.language === "zh" ? "/zhstatement" : "/enstatement",
+                "_blank"
+              )
+            }
+            className="cursor-pointer md:mt-5 link-hover"
+          >
+            {t("footer.viewBtn")}
+          </span>
         </div>
       </div>
     </div>
